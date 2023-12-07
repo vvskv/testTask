ec02d7dbe3b834a88d0a5e2275cecd2f978d5002
vvskv <116917067+vvskv@users.noreply.github.com>
2023-12-07 09:50:58 +0300

    add content

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <a href=""><img src="images/logo.svg" alt=""></a>
+        <nav>
+            <ul>
+                <li><a href="">Обо мне</a></li>
+                <li><a href="">Наставничество</a></li>
+                <li><a href="">Мероприятия</a></li>
+                <li><a href="">Кейсы</a></li>
+                <li><a href="">Отзывы</a></li>
+                <li><a href="">Контакты</a></li>
+            </ul>
+        </nav>
+        <a href="tel:8-345-123-34-45">8-345-123-34-45</a>
+    </header>
+    <main>
+        <span>Создаю условия для вашего успеха</span>
+        <p>Когда ваше время и энергия лучше сфокусированы, стремление к новым возможностям становится реальностью, ваш успех зависит от ваших действий</p>
+        <div>
+            <a href="">Записаться на консультацию</a>
+            <a href="">Бесплатная консультация</a>
+        </div>
+        <div>
+            <div>
+                <span>130+</span>
+                <p>техник для достижения целей</p>
+            </div>
+            <div>
+                <span>250%</span>
+                <p>увеличение личной продуктивности</p>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
