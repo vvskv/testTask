fab12fb6ec75d509765d124075e4095a23d87e9a
vvskv <vvskvorcov@yandex.ru>
2023-12-09 17:10:39 +0300

    add css

--- index.php
+++ index.php
@@ -7,42 +7,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Raleway:wght@400;600&display=swap" rel="stylesheet">
+
     <link rel="stylesheet" href="styles/style.css" />
 </head>
 
 <body>
-    <header>
-        <a href=""><img src="images/logo.svg" alt=""></a>
-        <nav>
-            <ul>
-                <li><a href="">Обо мне</a></li>
-                <li><a href="">Наставничество</a></li>
-                <li><a href="">Мероприятия</a></li>
-                <li><a href="">Кейсы</a></li>
-                <li><a href="">Отзывы</a></li>
-                <li><a href="">Контакты</a></li>
-            </ul>
-        </nav>
-        <a href="tel:8-345-123-34-45">8-345-123-34-45</a>
-    </header>
-    <main>
-        <span>Создаю условия для вашего успеха</span>
-        <p>Когда ваше время и энергия лучше сфокусированы, стремление к новым возможностям становится реальностью, ваш успех зависит от ваших действий</p>
-        <div>
-            <a href="">Записаться на консультацию</a>
-            <a href="">Бесплатная консультация</a>
-        </div>
-        <div>
-            <div>
-                <p>130+</p>
-                <p>техник для достижения целей</p>
+    <div class="bg-wrap">
+        <header class="container">
+            <a class="logo" href=""><img src="images/logo.svg" alt=""></a>
+            <nav>
+                <ul class="menu">
+                    <li><a href="">Обо мне</a></li>
+                    <li><a href="">Наставничество</a></li>
+                    <li><a href="">Мероприятия</a></li>
+                    <li><a href="">Кейсы</a></li>
+                    <li><a href="">Отзывы</a></li>
+                    <li><a href="">Контакты</a></li>
+                </ul>
+            </nav>
+            <a href="tel:8-345-123-34-45" class="menu-tel">
+                <img src="images/icon-tel.svg" alt="">
+                <p>8-345-123-34-45</p>
+            </a>
+        </header>
+
+        <main class="container">
+            <div class="mp-content">
+                <h1>Создаю условия <br> для вашего успеха</h1>
+
+                <div class="mp-content__text-box">
+                    <p>Когда ваше время и энергия лучше сфокусированы, стремление к новым <br> возможностям становится реальностью, ваш успех зависит от ваших действий</p>
+                </div>
+                
+                <div class="mp-content__btn">
+                    <a class="btn btn--white" href="">
+                        <span>Записаться на консультацию</span> </a>
+                    <a class="btn btn--opacity" href="">
+                        <span> Бесплатная консультация</span></a>
+                </div>
+
+                <div class="mp-content__advantage">
+                    <div class="mp-content__text-box">
+                        <span>130+</span>
+                        <p>техник для <br> достижения целей</p>
+                    </div>
+                    <div class="mp-content__text-box bg-lines">
+                        <span>250%</span>
+                        <p>увеличение личной <br> продуктивности</p>
+                    </div>
+                </div>
             </div>
-            <div>
-                <p>250%</p>
-                <p>увеличение личной продуктивности</p>
-            </div>
-        </div>
-    </main>
+            
+        </main>
+
+    </div>
+    
 </body>
 
 </html>
