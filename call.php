5272d5477355dc6285b9028939f820dfcbb4e74a
vvskv <vvskvorcov@yandex.ru>
2023-12-11 05:18:16 +0300

    c

--- call.php
+++ call.php
@@ -8,9 +8,9 @@ if(isset($_POST["name"])){
   
       $tel = $_POST["tel"];
   }
-//   if (
-//       mail("mail1@yandex.ru", "Заявка с сайта", "Имя: ".$name.". Телефон: ".$tel) && 
-//       mail("mail2@yandex.ru", "Заявка с сайта", "Имя: ".$name.". Телефон: ".$tel)
-//       ) {
+  if (
+      mail("mail1@yandex.ru", "Заявка с сайта", "Имя: ".$name.". Телефон: ".$tel) && 
+      mail("mail2@yandex.ru", "Заявка с сайта", "Имя: ".$name.". Телефон: ".$tel)
+      ) {
 
-//   };
+  };
